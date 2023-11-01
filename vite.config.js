5fc0431860e905f7ab41efcbb5ac73d8f9166e2e
De Roover Justin <justinderoover@gmail.com>
2023-11-01 17:02:13 +0100

    f

--- vite.config.js
+++ vite.config.js
@@ -2,4 +2,12 @@ import { defineConfig } from "vite";
 
 export default defineConfig({
   base: "/weather-app/",
+  build: {
+    rollupOptions: {
+      output: {
+        // Inclure le dossier "images" dans le chemin de base
+        assetFileNames: "images/",
+      },
+    },
+  },
 });
